94db173b3e8003a26c2bb80695264dfa3366c267
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-04 13:02:28 +0700

    add history

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import {Router} from "react-router-dom"
-import {createBrowserHistory} from "history"
-
-const history = createBrowserHistory()
+import history from "./history"
 
 ReactDOM.render(
   <React.StrictMode>
