5848771acd81985f0fd1dcae334de17801d18eeb
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-04 09:41:26 +0700

    start project

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "none",
+  tabWidth: 2,
+  printWidth: 120,
+  semi: false,
+  singleQuote: false,
+  jsxBracketSameLine: true,
+  bracketSpacing: false,
+  arrowParens: "always",
+  endOfLine: "lf"
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Simple } from "./components/simple";
+import { Function } from "./components/function";
+
+function App() {
+  return (
+    <>
+      <Simple />
+      <Function text="Hello" />
+    </>
+  );
+}
+
+export default App;
