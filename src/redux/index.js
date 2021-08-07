0a539548644f972cb4886b249e805bddd408d6eb
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-07 08:59:38 +0700

    add redux-saga

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,7 @@
 import {combineReducers} from "redux"
-import localData from "./reducer/local-data/local-data"
+
 import externalData from "./reducer/external-data/external-data"
 
 export default combineReducers({
-  // localData,
   externalData
 })
