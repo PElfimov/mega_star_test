9a17328581135643d5f006a65681b588b396b9eb
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-07 11:33:25 +0700

    move history to redusers

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,4 +1,7 @@
+import {createBrowserHistory} from "history"
 const initial = {}
+
+export const history = createBrowserHistory()
 
 export default function reducer(state: {} = initial, action) {
   return state
