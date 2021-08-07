eaf704ca6e0044f2de1c28938696ff3b7be8f1b8
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-07 15:32:55 +0700

    fix middleware

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,5 +1,5 @@
-import {createStore, applyMiddleware} from "redux"
-import {compose} from "recompose"
+import {createStore, applyMiddleware, compose} from "redux"
+
 import createSagaMiddleware from "redux-saga"
 import reducer, {history} from "./reducers"
 import rootSaga from "./sagas"
@@ -7,13 +7,12 @@ import {routerMiddleware} from "connected-react-router"
 
 const sagaMiddleware = createSagaMiddleware()
 
-const store = createStore(
-  reducer,
-  compose(
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ ? (window as any).__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-    applyMiddleware(routerMiddleware(history), sagaMiddleware)
-  )
-)
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  : compose
+
+const enhancer = composeEnhancers(applyMiddleware(routerMiddleware(history), sagaMiddleware))
+const store = createStore(reducer, enhancer)
 
 sagaMiddleware.run(rootSaga)
 
