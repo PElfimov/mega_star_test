f2d58162cb717058e31caee9ea8e7df67b53d41c
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-07 11:26:01 +0700

    add history

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,13 @@ import App from "./App"
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
 import store from "./redux"
+import {createBrowserHistory} from "history"
 
-// const api = configureAPI((...args) => store.dispatch({...args}))
+const history = createBrowserHistory()
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter history={history}>
       <App />
     </BrowserRouter>
   </Provider>,
