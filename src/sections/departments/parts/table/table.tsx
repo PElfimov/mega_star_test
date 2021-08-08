9a37185de52f9fab06eb7897e593fcffdd09ec4d
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-08 17:34:14 +0700

    add data to table

--- src/sections/departments/parts/table/table.tsx
+++ src/sections/departments/parts/table/table.tsx
@@ -1,6 +1,9 @@
 import {Department} from "../../../../lib/interfaces"
-import {Button, Table as BootstrapTable} from "react-bootstrap"
+import styles from "./table.module.css"
+import {Button, Spinner, Table as BootstrapTable} from "react-bootstrap"
 import {Dropdown} from "./parts/dropdown"
+import {useSelector} from "react-redux"
+import {selectDepartments} from "../../../../redux/reducers/departments/selectors"
 
 const DEPARTAMENTS: Department[] = [
   {id: 1, status: "active", name: `Development`, description: `lorem Lorem`},
@@ -9,34 +12,45 @@ const DEPARTAMENTS: Department[] = [
 ]
 
 export function Table() {
+  const departments = useSelector(selectDepartments)
+  const {loading, data} = departments
+
   return (
     <>
-      <BootstrapTable striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Status</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {DEPARTAMENTS.map((item) => {
-            return (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.status}</td>
-                <td>{item.name}</td>
-                <td>{item.description}</td>
-                <td>
-                  <Dropdown />
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </BootstrapTable>
+      {loading && (
+        <div className={styles.spinnerWrapper}>
+          <Spinner animation="border" />
+        </div>
+      )}
+      {!loading && (
+        <BootstrapTable striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Status</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.map((item) => {
+                return (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.status}</td>
+                    <td>{item.name}</td>
+                    <td>{item.description}</td>
+                    <td>
+                      <Dropdown />
+                    </td>
+                  </tr>
+                )
+              })}
+          </tbody>
+        </BootstrapTable>
+      )}
       <Button variant="primary">Add New</Button>
     </>
   )
