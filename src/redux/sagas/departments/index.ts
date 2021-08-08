176bd12d4c255b1700690c3cc5d255d4d79514c6
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-08 15:16:03 +0700

    create new saga departmets

---
+++ src/redux/sagas/departments/index.ts
@@ -0,0 +1,87 @@
+import {DEPARTMENTS} from "./../../reducers/departments/actions"
+import {DEPARTMENT_DETAILS} from "./../../reducers/departmentDetails/actions"
+import {getRoutingConfig, ROUTES_NAME} from "./../../../sections/app/routes"
+import {selectEmployees} from "./../../reducers/employees/selectors"
+import axios from "axios"
+import {LOCATION_CHANGE} from "connected-react-router"
+import {call, fork, take, put, takeEvery, select} from "redux-saga/effects"
+import {matchPath} from "react-router"
+
+function fetchUsers(id?: number) {
+  let path = `departments`
+  if (id) {
+    path = `departments/${id}`
+  }
+  return axios({
+    method: "get",
+    url: `${process.env.REACT_APP_API_DOMAIN}/${path}`
+  })
+}
+
+function* loadDepartmentDetails(data) {
+  const id = data.payload
+
+  const request = yield call(() => fetchUsers(id))
+  try {
+    if (`id` in request.data) {
+      yield put({
+        type: DEPARTMENT_DETAILS.LOAD_SUCCESS,
+        payload: request.data
+      })
+    } else
+      yield put({
+        type: DEPARTMENT_DETAILS.LOAD_FAILURE,
+        payload: request.data
+      })
+  } catch (error) {
+    yield put({
+      type: DEPARTMENT_DETAILS.LOAD_FAILURE,
+      payload: error
+    })
+  }
+}
+
+function* loadDepartmentsList() {
+  const request = yield call(fetchUsers)
+  const data = request.data
+  yield put({
+    type: DEPARTMENTS.LOAD_SUCCESS,
+    payload: data
+  })
+}
+
+export function* routeChange() {
+  while (true) {
+    const action = yield take(LOCATION_CHANGE)
+
+    const departmentsPage = matchPath(action.payload.location.pathname, getRoutingConfig(ROUTES_NAME.DEPARTMENTS))
+    console.log(departmentsPage)
+
+    if (departmentsPage) {
+      const state = yield select(selectEmployees)
+      const {page, search} = state
+      yield put({
+        type: DEPARTMENTS.LOAD,
+        payload: {page, search}
+      })
+    }
+
+    const detailsPage = matchPath(action.payload.location.pathname, getRoutingConfig(ROUTES_NAME.DEPARTMENT_DETAILS))
+    if (detailsPage) {
+      const {id} = detailsPage.params
+
+      if (id) {
+        yield put({
+          type: DEPARTMENT_DETAILS.LOAD,
+          payload: id
+        })
+      }
+    }
+  }
+}
+
+export default function* departmentsSaga() {
+  yield fork(routeChange)
+  yield takeEvery(DEPARTMENTS.LOAD, loadDepartmentsList)
+  yield takeEvery(DEPARTMENT_DETAILS.LOAD, loadDepartmentDetails)
+}
--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,10 +1,11 @@
 import {spawn, call, all} from "redux-saga/effects"
+import departmentsSaga from "./departments"
 import employeesSaga from "./employees"
 
 export default function* rootSaga() {
   console.log("Sagas")
 
-  const sagas = [employeesSaga]
+  const sagas = [employeesSaga, departmentsSaga]
 
   const retrySagas = yield sagas.map((saga) => {
     return spawn(function* () {
