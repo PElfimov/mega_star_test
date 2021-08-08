697e6a61245032068ba0ea0d3ad57fef914e931a
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-08 18:11:49 +0700

    refactoring navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -2,18 +2,30 @@ import {Nav} from "react-bootstrap"
 import {Link} from "react-router-dom"
 
 export function Navigation() {
+  const button = [
+    {
+      name: "Departments",
+      path: "/departments",
+      isActive: false
+    },
+    {
+      name: "Employees",
+      path: "/employees",
+      isActive: true
+    }
+  ]
+
   return (
     <Nav variant="pills" className="flex-column" activeKey="1">
-      <Nav.Item>
-        <Link className={`nav-link active`} to="/departments">
-          Departments
-        </Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Link className={`nav-link`} to="/employees">
-          Employees
-        </Link>
-      </Nav.Item>
+      {button.map((item) => {
+        return (
+          <Nav.Item key={item.name}>
+            <Link className={`nav-link ${item.isActive && `active`}`} to={item.path}>
+              {item.name}
+            </Link>
+          </Nav.Item>
+        )
+      })}
     </Nav>
   )
 }
